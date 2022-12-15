99fe0c0ccc693b69b42c90a39672b09b4dbeab81
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-12-15 12:07:31 +0100

    log: sentry config

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const passport = require("passport");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -12,6 +14,21 @@ require("./src/authentication/passport");
 const app = express();
 
 // Middleware
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	integrations: [
+		// enable HTTP calls tracing
+		new Sentry.Integrations.Http({ tracing: true }),
+		// enable Express.js middleware tracing
+		new Tracing.Integrations.Express({ app }),
+	],
+
+	// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+	tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	max: 100,
@@ -20,7 +37,9 @@ const limiter = rateLimit({
 	message: "Too many requests, please try again after 15 minutes",
 });
 app.use(limiter);
+
 app.use(helmet());
+
 app.use(express.json());
 
 // Routes
@@ -41,10 +60,13 @@ app.use("*", (req, res) => {
 	return res.status(404).json({ message: "Route not found" });
 });
 
-// Error Handler
-app.use(function (err, req, res, next) {
+// Sentry error handler
+app.use(Sentry.Handlers.errorHandler());
+
+// Fallthrough Error Handler
+app.use(function onError(err, req, res, next) {
 	// console.log(err);
-	return res.status(err.status || 500).json({ error: err.message });
+	return res.status(err.status || 500).end(res.sentry + "\n");
 });
 
 module.exports = app;
