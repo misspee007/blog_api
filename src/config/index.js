1dfc42a770e291c5920ef4f9ebc4807a71fded42
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-12-28 12:13:41 +0100

    enhancement(logging): replace sentry with morgan & winston feat: oauth

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,8 @@ module.exports = {
 	PORT: process.env.PORT,
 	MONGODB_CONNECTION_URL: process.env.MONGODB_CONNECTION_URL,
 	JWT_SECRET: process.env.JWT_SECRET,
-	SENTRY_DSN: process.env.SENTRY_DSN,
+  AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+  AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_SECRET: process.env.AUTH0_SECRET,
 };
